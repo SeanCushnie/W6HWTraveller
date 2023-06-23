13d4def9f9f55bee2194c26f055b9ff2f6f883ac
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-06-23 17:47:38 +0100

    first three tests

--- models/traveller.js
+++ models/traveller.js
@@ -7,11 +7,11 @@ return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  return this.journeys.map(journey => journey.endLocation);
+return this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+return this.journeys.filter(journey => journey.transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
